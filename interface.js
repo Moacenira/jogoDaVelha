04c2ab82e62faf9aa31feae040e9534d8c1afb76
Moacenira <cenira.cardoso@gmail.com>
2021-11-14 15:27:00 -0300

    update

---
+++ interface.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', () => {
+  let squares = document.querySelectorAll('.square')
+  squares.forEach(square => {
+    square.addEventListener('click', handleClick)
+  })
+})
+function handleClick(event) {
+  let square = event.target
+  let position = square.id
+
+  if (handleMove(position)) {
+      setTimeout(() => {
+      alert('o jogo acabou \n O vencedor foi o jogador ' + playerTime)
+    }, 10);
+  };
+  updateSquares();
+}
+function updateSquares(position){
+  let square=document.getElementById(position.toString());
+  let symbol = board[position];
+  square.innerHTML = `<div class='${symbol}'></div>`
+
+}
+
+function updateSquares() {
+  let squares = document.querySelectorAll('.square')
+  squares.forEach(square => {
+    let position = square.id
+    let symbol = board[position]
+
+    if (Symbol != '') {
+      square.innerHTML = `<div class='${symbol}'></div>`
+    }
+  })
+}
